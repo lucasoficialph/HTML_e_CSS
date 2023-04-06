bded6f28f2de622c179c5494154c24aa512559ff
Lucas Gabriel <lucas.oficial.tube@gmail.com>
2023-04-05 21:23:17 -0300

    Update JavaScript.js

--- PROJETOS/Projeto-Site-Moderno/js/JavaScript.js
+++ PROJETOS/Projeto-Site-Moderno/js/JavaScript.js
@@ -1,5 +1,5 @@
 window.sr = ScrollReveal({reset: true})
 
 ScrollReveal().reveal('.textmodelo_de_home_page', {
-    delay: 1000, rotate:{x:100, y:0, z:0}
+    delay: 1000, rotate:{x:50, y:0, z:0}, position:{x:100, y:0, z:0}
 })
